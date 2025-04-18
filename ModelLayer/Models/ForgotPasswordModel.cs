766b46be9ffc9f4e97ce31b5e8e2595da2b385f4
Harireddy123 <hariprasadreddy413381@gmail.com>
2025-04-18 15:54:16 +0530

    Add .vs/ to .gitignore

---
+++ ModelLayer/Models/ForgotPasswordModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ModelLayer.Models
+{
+    public class ForgotPasswordModel
+    {
+        [Required(ErrorMessage = "User ID is required")]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Token is required")]
+        public string Token { get; set; }
+    }
+}
