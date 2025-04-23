68e5d565edb15db78a8451fbcebc888d2c245b0b
Harireddy123 <hariprasadreddy413381@gmail.com>
2025-04-23 10:35:19 +0530

    [Hari]Added: Added Book Model

---
+++ ModelLayer/Models/BookModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal DiscountPrice { get; set; }
+        public int Quantity { get; set; }
+        public string BookImage { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
